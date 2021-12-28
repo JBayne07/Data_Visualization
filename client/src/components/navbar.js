30682d7af3889725c9ddd26f2db83210c121ac91
namoosir <saqeebnazmus@gmail.com>
2021-12-27 21:40:06 -0500

    Add theme and make menu better

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -3,40 +3,35 @@ import { Link} from 'react-router-dom';
 // import { Link } from '@mui/material';
 import { AppBar, Box, Toolbar, Typography, Button, IconButton} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 
 export const Navbar = () => {
+
     return (
-        <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{ flexGrow: 2 }}>
           <AppBar position="static">
             <Toolbar>
-              <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Typography variant="h4" component="div" sx={{ flexGrow: 2 }}>
                 Algorithm Visualization
               </Typography>
-              <Button variant="contained" color="inherit">               
-                  <Link to='/' className='HomePage' color="inherit">
-                    Home
-                  </Link>
-              </Button>
-              <Button variant="contained" color="inherit">               
-                  <Link to='/sort' className='SortPage' color="inherit">
-                    Sorting
-                  </Link>
-              </Button>
-              <Button variant="contained" color="inherit">
-                <Link to='/graph' className='GraphPage' color="inherit">
-                  Graph
+              <MenuItem>
+                <Link to='/' className='HomePage' color="inherit" style={{"text-decoration":"none", "color":"white"}}>
+                  HOME
                 </Link>
-              </Button>
+              </MenuItem>
+
+              <MenuItem>
+                <Link to='/sort' className='HomePage' color="inherit" style={{"text-decoration":"none", "color":"white"}}>
+                  SORTING
+                </Link>
+              </MenuItem>
+
+              <MenuItem>
+                <Link to='/graph' className='HomePage' color="inherit" style={{"text-decoration":"none", "color":"white"}}>
+                  GRAPH
+                </Link>
+              </MenuItem>
             </Toolbar>
           </AppBar>
         </Box>
---
+++ client/src/components/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#69e2ff',
+      main: '#00b0ff',
+      dark: '#0081cb',
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#73e8ff',
+      main: '#29b6f6',
+      dark: '#0086c3',
+      contrastText: '#000000',
+    },
+  },
+});
